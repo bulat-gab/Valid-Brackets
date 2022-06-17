062513faa2f3eaf52f9ec53e446bafcf2b802613
Bulat Gabdrakhmanov <v-bulatgmicrosoft.com>
2022-06-17 12:05:26 +0700

    Update using stack

---
+++ ValidBrackets.cs
@@ -0,0 +1,56 @@
+﻿// String [{()}]
+// { ( } ) 
+
+// curlyBrackets: 1; parenthesis: 1
+
+namespace ValidBrackets;
+
+public class Program
+{
+    Dictionary<char, char> bracketsMapping = new Dictionary<char, char>
+    {
+        { ']', '[' },
+        { '}', '{' },
+        { ')', '(' },
+    };
+
+    public bool IsCorrect(string input)
+    {
+        var openingBrackets = new List<char> { '[', '{', '(' };
+        var closingBrackets = new List<char> { ']', '}', ')' };
+
+        if (string.IsNullOrEmpty(input))
+            return false;
+
+        var stack = new Stack<char>();
+        foreach (char currentBracket in input)
+        {
+            if (openingBrackets.Contains(currentBracket))
+            {
+                stack.Push(currentBracket);
+                continue;
+            }
+            else if (closingBrackets.Contains(currentBracket))
+            {
+                // The closing bracket does not have a corresponding opening bracket, i.e. the input is invalid.
+                if (!stack.TryPop(out var previousBracket))
+                {
+                    return false;
+                }
+
+                // The closing and opening brackets do not match.
+                if (bracketsMapping[currentBracket] != previousBracket)
+                {
+                    return false;
+                }
+            }
+            // The input contains unexpected characters.
+            else
+            {
+                return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
---
+++ ValidBracketsTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace ValidBrackets;
+
+[TestFixture]
+public class ProgramTest
+{
+    private Program program = new Program();
+
+    [Test]
+    [TestCase("[{()}]")]
+    [TestCase("[]")]
+    [TestCase("(({}))")]
+    public async Task IsCorrect_ShouldReturnTrue_ForValidInput(string input)
+    {
+        var result = program.IsCorrect(input);
+
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    [TestCase("")]
+    [TestCase(null)]
+    [TestCase("[}")]
+    [TestCase("[[")]
+    [TestCase("[(])")]
+    public async Task IsCorrect_ShouldReturnFalse_ForInvalidInput(string input)
+    {
+        var result = program.IsCorrect(input);
+
+        Assert.IsFalse(result);
+    }
+}
